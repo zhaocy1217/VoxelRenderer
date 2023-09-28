8d13bdec78248404c60f7923f735bb5729eec409
chaoyuezhao <zcyandy@outlook.com>
2023-09-28 17:50:59 +0800

    submodule issue

---
+++ update_submodule_manually.bat
@@ -0,0 +1,8 @@
+git submodule add https://github.com/assimp/assimp.git third/assimp
+git submodule add https://github.com/g-truc/glm.git third/glm
+git submodule add https://github.com/tinyobjloader/tinyobjloader.git third/tinyobjloader
+git submodule add https://github.com/jbeder/yaml-cpp.git third/yaml-cpp
+git submodule add https://github.com/ocornut/imgui.git third/imgui
+git submodule add https://github.com/KhronosGroup/glTF.git third/glTF
+git submodule add https://github.com/glfw/glfw.git third/glfw
+
